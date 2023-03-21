1b2092aa1cb7bdb7cb2b383368cb66f17d19dc73
Bernardo <102758107+Bern4rd0e@users.noreply.github.com>
2023-03-21 18:31:33 -0300

    criação da pasta de repetição e resolução de exercício

---
+++ Repeticao/exercicio001/script.js
@@ -0,0 +1,34 @@
+function contar(){
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+ 
+
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length ==0){
+        window.alert('[ERRO] Faltam dados')
+        res.innerHTML = 'Impossível contar!'
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if(p <= 0){
+            window.alert('Passo inválido!Considerando PASSO 1')
+            P = 1
+    }
+        //contagem crescente 
+        if(i < f){
+            for(let c = i; c <= f; c += p){
+            res.innerHTML += `${c} \u{1F449}`
+        }
+        //contagem regressiva
+        } else {
+            for(let c = i; c >= f; c-= p){
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+    }
+}
---
+++ Repeticao/script.js
@@ -0,0 +1,18 @@
+ //var c = 1
+ 
+// while(c < 6){
+//     console.log(`${c}`)
+//     c++
+// } 
+
+// do {
+//     console.log(`Passo ${c}`)
+//     c++
+// } while (c <= 6) 
+
+
+console.log('VAI COMEÇAR')
+for(var c = 1; c <= 5; c++){
+    console.log(c)
+}
+console.log('FIM!')
